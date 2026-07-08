d817b83a412e07ff74a6a2cd39e8a1cc1adb11ce
Kalyan Biswas <160340582+kalyan-biswas@users.noreply.github.com>
2026-07-08 14:17:53 +0600

    feature: Added input validation

--- Projects/01_check_odd_even.c
+++ Projects/01_check_odd_even.c
@@ -3,7 +3,11 @@ int main(int argc, char const *argv[])
 {
   int num;
   printf("Enter a number: ");
-  scanf("%d", &num);
+  if (scanf("%d", &num) != 1)
+  {
+    printf("Error: Please enter a valid integer!\n");
+    return 1;
+  }
   if (num % 2 == 0)
   {
     printf("The number is even!\n");
